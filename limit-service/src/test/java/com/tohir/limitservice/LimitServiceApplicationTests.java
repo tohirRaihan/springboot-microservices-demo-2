953924fb7abb49fb985c00b75424db848b07b3ea
Tohir Raihan <tohir.raihan@gmail.com>
2024-03-31 22:41:52 +0600

    :pencil2: Add: added limit-service microservice

---
+++ limit-service/src/test/java/com/tohir/limitservice/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tohir.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
